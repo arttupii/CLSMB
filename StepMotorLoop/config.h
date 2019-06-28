fa96a3120675cb85299d0bae1984a4117cde7fbe
arttupii <arttupii@gmail.com>
2019-06-28 15:00:23 +0300

    Update config.h

--- StepMotorLoop/config.h
+++ StepMotorLoop/config.h
@@ -1,7 +1,9 @@
 #ifndef __CONFIG__H_
 #define __CONFIG__H_
 
-#define ENCODER_PPR 1024 //Pulses per Revolution
+#define ENCODER_PPR 1024 //Pulses per Revolution (AS5040)
+//#define ENCODER_PPR 400 //Pulses per Revolution (HN3806-AB -400N)
+
 
 #define STEP_0_PULSE_US 30
 #define STEP_1_PULSE_US 30

28bf1e28d25c6095610e9c8d7a25c1e09a8eb479
arttupii <arttupii@gmail.com>
2019-07-05 15:09:30 +0300

    Backup

--- StepMotorLoop/config.h
+++ StepMotorLoop/config.h
@@ -3,18 +3,18 @@
 
 #define ENCODER_STATE_CHANGE_PER_REV 1024.0 //State change per Revolution (A+B) (AS5040)
 
-#define STEP_MOTOR_REV_PER_SEC 4 //revolutions/second. Fixing speed
+#define STEP_MOTOR_REV_PER_SEC 3.0 //revolutions/second. Fixing speed
 
 #define NEMA_MOTOR_FULL_STEPS 200.0
 #define NEMA_MOTOR_MICROSTEPS 16.0
 #define NEMA_MOTOR_PPR (NEMA_MOTOR_FULL_STEPS * NEMA_MOTOR_MICROSTEPS)
 
 #define STEP_ERROR_MAX 20 //Start position repairing if position deviation is more than this
-#define STEP_ERROR_MIN 1 //Stop position repairing if position deviation is less than this
+#define STEP_ERROR_MIN 3 //Stop position repairing if position deviation is less than this
 
 //#define FLIP_ENCODER //enable if rotor position repairing is done wrong direction
 
-#define ENABLE_PRINTS 0
+#define ENABLE_PRINTS 1
 
 #define STEP_1_PULSE_US 1 //pulse wide
 
